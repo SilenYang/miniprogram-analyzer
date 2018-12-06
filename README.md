b3fecf9dc5b67c566ef15107ba02806d04c6f5b7
SilenYang <yangyh19930308@gmail.com>
2018-12-06 10:55:02 +0800

    add parameter path

--- README.md
+++ README.md
@@ -1,39 +1,49 @@
 # miniprogram-analyzer
+
 a command-line tools for WeChat miniprogram to analyze your project
 
 # install
+
 ```js
 npm install miniprogram-analyzer -g
 ```
 
 # usage
+
 安装完成后可在相应的小程序目录使用`wma`命令开始分析你的项目
 
 目前仅有两个功能：分析文件和查找项目中未使用的图片(仅做参考)
 
 ### 分析文件
+
 ```js
 wma check [option]
 
 option:
     -S, --size  按文件大小进行筛选,单位为KB
     --sort      按文件大小排序(降序)
+    -P, --path  指定检查路径，默认为当前目录
 ```
+
 ![check](./images/check.jpg)
 
 ![check](./images/check_filter.jpg)
 
 ### 查找图片
+
 ```js
 wma image_check|image [option]
 
 option:
     -S, --sort      按文件大小进行排序(降序)
     -D, --delete    查找图片并删除(谨慎使用)
+    -P, --path  指定检查路径，默认为当前目录
 ```
+
 ![image](./images/image.jpg)
 
 ![image_filter](./images/image_filter.jpg)
 
 # LICENSE
+
 MIT
--- index.js
+++ index.js
@@ -1,86 +1,92 @@
 #! /usr/bin/env node
 
-const path = require('path')
-const program = require('commander')
-const ora = require('ora')
-const fs = require('fs')
-const chalk = require('chalk')
-const table = require('painting-table')
-const { checkUselessImage, deleteFiles, AllFiles } = require('./lib/wx_tools')
+const path = require("path");
+const program = require("commander");
+const ora = require("ora");
+const fs = require("fs");
+const chalk = require("chalk");
+const table = require("painting-table");
+const { checkUselessImage, deleteFiles, AllFiles } = require("./lib/wx_tools");
+
+program.version("0.0.1").usage("<cmd> [options]");
 
 program
-    .version('0.0.1')
-    .usage('<cmd> [options]')
-
-program
-    .command('image_check')
-    .alias('image')
-    .option('-D, --delete', '删除查找出来的图片(谨慎使用)')
-    .option('-S, --sort', '按文件大小进行排序')
-    .description('查找没有使用过的图片')
-    .action(function(options) {
-        const spinner = ora('checking...')
-        spinner.start()
-        checkUselessImage(options.sort).then(res => {
-            /* 
-                [{
-                    path: '',  文件路径
-                    filename: '', 文件名称
-                    size: 0, 文件大小
-                    modify_time: '' 上次修改日期
-                }]
-            */
-            spinner.stop()
-            options.delete && deleteFiles(res)
-            res.forEach(item => item.path = item.path.replace(process.cwd(), ''))
-            const option = {
-                excludes: [],
-                includes: { filename: true, size: true, modify_time: false },
-                rename: {}
-            }
-            const str = table(res, option)
-            console.log(str)
-        })
-    }).on('--help', function() {
-        console.log()
-        console.log('  Examples:')
-        console.log()
-        console.log('    $ deploy image_check|image')
-        console.log('    $ deploy image_check|image -s -d')
-        console.log()
+  .command("image_check")
+  .alias("image")
+  .option("-D, --delete", "删除查找出来的图片(谨慎使用)")
+  .option("-S, --sort", "按文件大小进行排序")
+  .option("-P, --path", "指定检查路径，默认为当前目录")
+  .description("查找没有使用过的图片")
+  .action(function(options) {
+    console.log(process.cwd(), options);
+    const spinner = ora("checking...");
+    spinner.start();
+    checkUselessImage(options.sort, options.path).then(res => {
+      /* 
+        [{
+          path: '',  文件路径
+          filename: '', 文件名称
+          size: 0, 文件大小
+          modify_time: '' 上次修改日期
+        }]
+      */
+      spinner.stop();
+      options.delete && deleteFiles(res);
+      res.forEach(item => (item.path = item.path.replace(process.cwd(), "")));
+      const option = {
+        excludes: [],
+        includes: {
+          filename: true,
+          size: true,
+          path: true,
+          modify_time: false
+        },
+        rename: {}
+      };
+      const str = table(res, option);
+      console.log(str);
     });
+  })
+  .on("--help", function() {
+    console.log();
+    console.log("  Examples:");
+    console.log();
+    console.log("    $ deploy image_check|image");
+    console.log("    $ deploy image_check|image -S -D");
+    console.log();
+  });
 
 program
-    .command('check')
-    .option('-S, --size <num>', '按文件大小进行筛选(kb)')
-    .option('--sort', '按文件大小排序')
-    .description('显示所有文件信息，除了图片')
-    .action(function(options) {
-        const spinner = ora('checking...')
-        spinner.start()
-        AllFiles(options.size, options.sort).then(res => {
-            spinner.stop()
-            const option = {
-                excludes: [],
-                includes: { filename: true, path: true, size: true },
-                rename: {}
-            }
-            const str = table(res, option)
-            console.log(str)
-        })
-    }).on('--help', function() {
-        console.log()
-        console.log('  Examples:')
-        console.log()
-        console.log('    $ deploy check')
-        console.log('    $ deploy check -s 2 --sort')
-        console.log()
+  .command("check")
+  .option("-S, --size <num>", "按文件大小进行筛选(kb)")
+  .option("--sort", "按文件大小排序")
+  .option("-P, --path", "指定检查路径，默认为当前目录")
+  .description("显示所有文件信息，除了图片")
+  .action(function(options) {
+    const spinner = ora("checking...");
+    spinner.start();
+    AllFiles(options.size, options.sort, options.path).then(res => {
+      spinner.stop();
+      const option = {
+        excludes: [],
+        includes: { filename: true, path: true, size: true },
+        rename: {}
+      };
+      const str = table(res, option);
+      console.log(str);
     });
+  })
+  .on("--help", function() {
+    console.log();
+    console.log("  Examples:");
+    console.log();
+    console.log("    $ deploy check");
+    console.log("    $ deploy check -S 2 --sort");
+    console.log();
+  });
 
-program
-    .command('*')
-    .action((env) => {
-        console.error(chalk.red(`不存在命令: "${env}"`));
-    });
+program.command("*").action(env => {
+  console.error(chalk.red(`不存在命令: "${env}"`));
+});
 
 program.parse(process.argv);
